e168f97ac5a67c9120ec74322bcb4773038c0247
Budi Gandasoebrata & Gabe Hollombe <pair+budi+gabe@veritrans.co.id>
2014-04-07 17:38:55 +0700

    First commit. Fresh rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Veritweet::Application.config.secret_key_base = '9595307095f397098d69331dfb0034805d5a13bcdcc62fd414190030ac42facb802d43cbf53a91b85024664d31dedd22e70f0a553cf43f399078294b34258909'
